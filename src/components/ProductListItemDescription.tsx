5557874e08b9c42eaebf9f445ed738f23b5b6eaa
Daniel Noworyta <daniel.noworyta@gmail.com>
2023-09-06 22:35:25 +0200

    Working on product list

---
+++ src/components/ProductListItemDescription.tsx
@@ -0,0 +1,27 @@
+import { formatMoney } from "@/lib/utils";
+import { type Product } from "@/model/product";
+
+type ProductListItemDescriptionProps = {
+	product: Product;
+};
+
+export const ProductListItemDescription = ({
+	product: { name, category, description, price },
+}: ProductListItemDescriptionProps) => {
+	return (
+		<div className="mt-2 flex justify-between">
+			<div>
+				<h2 className="text-sm font-semibold text-zinc-700">{name}</h2>
+				<p className="text-sm text-zinc-500">
+					<span className="sr-only">Kategoria: </span>
+					{category}
+				</p>
+				<p className="text-sm text-zinc-500">{description}</p>
+			</div>
+			<p className="text-sm font-medium text-zinc-900">
+				<span className="sr-only">Cena:</span>
+				{formatMoney(price / 100)}
+			</p>
+		</div>
+	);
+};
--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,6 +1,13 @@
-import { type ClassValue, clsx } from "clsx"
-import { twMerge } from "tailwind-merge"
- 
+import { clsx, type ClassValue } from "clsx";
+import { twMerge } from "tailwind-merge";
+
 export function cn(...inputs: ClassValue[]) {
-  return twMerge(clsx(inputs))
+	return twMerge(clsx(inputs));
+}
+
+export function formatMoney(amount: number, currency = "USD") {
+	return new Intl.NumberFormat("en-US", {
+		style: "currency",
+		currency,
+	}).format(amount);
 }
