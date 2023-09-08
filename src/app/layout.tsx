001be766c58084a1f59a1700976ed2635bb4d3b1
Daniel Noworyta <daniel.noworyta@gmail.com>
2023-09-08 07:17:56 +0200

    Finish week1

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -11,8 +11,12 @@ export const metadata: Metadata = {
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
 	return (
-		<html lang="en">
-			<body className={inter.className}>{children}</body>
+		<html lang="pl">
+			<body className={inter.className}>
+				<section className="mx-auto max-w-md p-12 sm:max-w-2xl sm:py-16 md:max-w-4xl lg:max-w-7xl">
+					{children}
+				</section>
+			</body>
 		</html>
 	);
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,3 @@
-export default function Home() {
-	return <section className="mx-auto max-w-md p-12 sm:max-w-2xl">Hello from awesome shop</section>;
+export default function HomePage() {
+	return <section>Hello from awesome shop</section>;
 }
