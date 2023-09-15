968ff429d0e43b59ec10584ad5b6a1f13c92e663
Daniel Noworyta <daniel.noworyta@gmail.com>
2023-09-15 06:46:54 +0200

    Update

--- src/app/api/products.ts
+++ src/app/api/products.ts
@@ -17,28 +17,36 @@ type Rating = {
 };
 
 export const getProductsList = async () => {
-	const res = await fetch("https://naszsklep-api.vercel.app/api/products");
-	const productsResponse = (await res.json()) as ProductFromResponse[];
-	const products = productsResponse.map((product): Product => {
-		return {
-			id: product.id,
-			name: product.title,
-			price: product.price,
-			description: product.description,
-			category: product.category,
-			image: {
-				src: product.image,
-				alt: product.title,
-			},
-		};
-	});
+	const res = await fetch(
+		"https://naszsklep-api.vercel.app/api/products",
+	);
+	const productsResponse =
+		(await res.json()) as ProductFromResponse[];
+	const products = productsResponse.map(
+		(product): Product => {
+			return {
+				id: product.id,
+				name: product.title,
+				price: product.price,
+				description: product.description,
+				category: product.category,
+				image: {
+					src: product.image,
+					alt: product.title,
+				},
+			};
+		},
+	);
 
 	return products;
 };
 
 export const getProductById = async (id: string) => {
-	const res = await fetch(`https://naszsklep-api.vercel.app/api/products/${id}`);
-	const productResponse = (await res.json()) as ProductFromResponse;
+	const res = await fetch(
+		`https://naszsklep-api.vercel.app/api/products/${id}`,
+	);
+	const productResponse =
+		(await res.json()) as ProductFromResponse;
 	const product: Product = {
 		id: productResponse.id,
 		name: productResponse.title,
@@ -54,24 +62,31 @@ export const getProductById = async (id: string) => {
 	return product;
 };
 
-export const getProductsPaginated = async (page: number) => {
+export const getProductsPaginated = async (
+	page: number,
+) => {
 	const res = await fetch(
-		`https://naszsklep-api.vercel.app/api/products?take=20&offset=${page * 20}`,
+		`https://naszsklep-api.vercel.app/api/products?take=20&offset=${
+			page === 1 ? 0 : page * 20
+		}`,
 	);
-	const productsResponse = (await res.json()) as ProductFromResponse[];
-	const products = productsResponse.map((product): Product => {
-		return {
-			id: product.id,
-			name: product.title,
-			price: product.price,
-			description: product.description,
-			category: product.category,
-			image: {
-				src: product.image,
-				alt: product.title,
-			},
-		};
-	});
+	const productsResponse =
+		(await res.json()) as ProductFromResponse[];
+	const products = productsResponse.map(
+		(product): Product => {
+			return {
+				id: product.id,
+				name: product.title,
+				price: product.price,
+				description: product.description,
+				category: product.category,
+				image: {
+					src: product.image,
+					alt: product.title,
+				},
+			};
+		},
+	);
 
 	return products;
 };
